a7cbbfce49724ab47ac96e7d1574540cea1af061
dimario9122 <72135810+dimario9122@users.noreply.github.com>
2021-12-18 00:07:10 +0300

    Add files via upload
    #3

---
+++ lab.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Lab
+{
+	class Program
+{
+    static void Main(string[] args)
+    {
+        Baker baker = new CreamBaker("КремВсем");
+        Cake cake = baker.Bake();
+        
+        Console.WriteLine();
+         
+        baker = new FruitBaker("ФруктНашПродукт");
+        Cake cake2 = baker.Bake();
+ 
+        Console.ReadLine();
+    }
+}
+	
+abstract class Baker
+{
+    public string Name { get; set; }
+ 
+    public Baker (string n)
+    { 
+        Name = n; 
+    }
+    
+    abstract public Cake Bake();
+}
+
+class CreamBaker : Baker
+{
+    public CreamBaker(string n) : base(n)
+    { }
+ 
+    public override Cake Bake()
+    {
+        return new CreamCake();
+    }
+}
+
+class FruitBaker : Baker
+{ 
+    public FruitBaker(string n) : base(n)
+    { }
+ 
+    public override Cake Bake()
+    {
+        return new FruitCake();
+    }
+}
+ 
+abstract class Cake
+{ }
+ 
+class FruitCake : Cake 
+{ 
+    public FruitCake()
+    {
+        Console.WriteLine("Фруктовый торт испечен");
+    }
+}
+class CreamCake : Cake
+{ 
+    public CreamCake()
+    {
+        Console.WriteLine("Кремовый торт испечен");
+    }
+}
+}
