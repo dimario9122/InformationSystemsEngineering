b745dd9ccb6eb85e05c01a840064333c1cad601c
dimario9122 <72135810+dimario9122@users.noreply.github.com>
2021-12-31 00:30:41 +0300

    #4

---
+++ ProgramCallback.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class MyMath
+{
+    public int Ceiling(double value)
+    {
+        return 13;
+    }
+}
+
+public class Arifmetic
+{
+    public static int MyFunc(double value, MyMath mymath)
+    {
+        double a = mymath.Ceiling(value);
+        Console.WriteLine(a);
+        return 0;
+    }
+
+    public static void Main()
+    {
+        MyMath math = new MyMath();
+        int v = MyFunc(12.69, math);
+    }
+}
