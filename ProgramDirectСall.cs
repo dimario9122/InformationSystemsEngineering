d68fbbe374c0d133e55f626a4f34b474029186b6
dimario9122 <72135810+dimario9122@users.noreply.github.com>
2021-12-31 00:31:09 +0300

    #4

---
+++ ProgramDirectСall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+public class Arifmetic
+{
+    public static int MyFunc(double value)
+    {
+        double a = Math.Ceiling(value);
+        Console.WriteLine(a);
+        return 0;
+    }
+
+    public static void Main()
+    {
+        int v = MyFunc(12.69);
+    }
+}
